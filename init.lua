8639c9331d456a7aadc1a7c7f4bd610665f1bfd0
Kaspar Rosin <kaspar.rosin@gmail.com>
2024-04-22 20:41:53 +0300

    Revert "feat: add comment plugin"
    This reverts commit 5d824c1b8fcfa4af45c2df68f7fb4c52a6ba719e.

--- init.lua
+++ init.lua
@@ -295,7 +295,7 @@ require('lazy').setup({
             return vim.list_extend(args, { '--hidden', '--no-ignore' })
           end,
         }
-      end, { desc = '[S]earch current [W]ord ' })
+      end, { desc = '[S]earch current [W]ord in all files' })
 
       vim.keymap.set('n', '<leader>sW', function()
         builtin.grep_string {
@@ -738,11 +738,6 @@ require('lazy').setup({
       --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
-  },
-  {
-    'numToStr/Comment.nvim',
-    opts = {},
-    lazy = false,
   },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
